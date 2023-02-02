b3fd88731dedbe131bc2eb64cab2b4729977fb90
Cassidy-Sowards <crsowards@outlook.com>
2023-02-02 17:22:45 -0500

    Added dark mode toggle //TODO add JS function

--- scripts.js
+++ scripts.js
@@ -3,10 +3,12 @@ addEventListener("scroll", () => {
     if(window.scrollY > 400){
         document.getElementById("nav").classList.replace("opacity-0", "opacity-100");
         document.getElementById("nav").classList.replace("h-0", "h-fit");
+        document.getElementById("nav").classList.replace("hidden", "visible");
     }
     else if(window.scrollY <= 100){
         document.getElementById("nav").classList.replace("opacity-100", "opacity-0");
         document.getElementById("nav").classList.replace("h-fit", "h-0");
+        document.getElementById("nav").classList.replace("visible", "hidden")
         
     }
 });
